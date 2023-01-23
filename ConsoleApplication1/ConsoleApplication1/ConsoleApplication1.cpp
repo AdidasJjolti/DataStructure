a3a76b62c00740a04e10ff552d874267c07f45b4
AdidasJjolti <118457300+AdidasJjolti@users.noreply.github.com>
2023-01-23 23:44:07 +0900

    STL 활용하여 stack, queue 구현
    - stack, queue STL 활용하여 stack, queue 구현, 멤버 함수 사용
      (직접 만든 함수와 비교하면 멤버 함수는 size가 0일 때 pop할 수 없음)

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,17 @@
 ﻿#include <iostream>
 #include "Stack.h"
 #include "Queue.h"
+#include<stack>
+#include<queue>
 
 #define MAX_SIZE 5
 #define NULL 0
 
 using namespace std;
+
+int x;
+stack<int> st;
+queue<int> qu;
 
 int main()
 {
@@ -27,6 +33,33 @@ int main()
 	stack.Pop();
 	*/
 
+	cout << "If empty? is 1, it's empty. If not, not empty." << endl;
+	cout << "Empty? " << st.empty() << endl;
+	st.push(1);
+	cout << st.top() << " is pushed." << endl;
+	st.push(2);
+	cout << st.top() << " is pushed." << endl;
+	cout << "Size is " << st.size() << endl;
+	cout << st.top() << " is popped." << endl;
+	st.pop();
+	st.push(3);
+	cout << st.top() << " is pushed." << endl;
+	st.push(4);
+	cout << st.top() << " is pushed." << endl;
+	st.push(5);
+	cout << st.top() << " is pushed." << endl;
+	cout << "Size is " << st.size() << endl;
+	int size = st.size();
+	for (int i = 0; i < size; i++)
+	{
+		cout << st.top() << " is popped." << endl;
+		st.pop();
+	}
+	cout << "Size is " << st.size() << endl;
+	cout << "Empty? " << st.empty() << endl;
+	cout << "'-----------------" << endl;
+
+	/*
 	Queue queue;
 	int data;
 
@@ -54,6 +87,35 @@ int main()
 	data = queue.Dequeue();
 	cout << "[Dequeue] : " << data << endl;
 	queue.GetSize();
+	*/
+
+	cout << "The size of qu is " << qu.size() << endl;
+	qu.push(1);
+	cout << qu.back() << " is pushed." << endl;
+	qu.push(2);
+	cout << qu.back() << " is pushed." << endl;
+	qu.push(3);
+	cout << qu.back() << " is pushed." << endl;
+	cout << "Size is " << qu.size() << endl;
+	cout << "Front is " << qu.front() << ", Rear is " << qu.back() << endl;
+	qu.pop();
+	cout << "Something is popped." << endl;
+	cout << "Front is " << qu.front() << ", Rear is " << qu.back() << endl;
+	qu.push(4);
+	cout << qu.back() << " is pushed." << endl;
+	qu.push(5);
+	cout << qu.back() << " is pushed." << endl;
+	qu.push(6);
+	cout << qu.back() << " is pushed." << endl;
+	cout << "Front is " << qu.front() << ", Rear is " << qu.back() << endl;
+
+	int qsize = qu.size();
+	for (int i=0; i < qsize; i++ )
+	{
+		cout << qu.front() << " is popped." << endl;
+		qu.pop();
+	}
+	cout << "Empty? " << qu.empty() << endl;
 
 
 	return 0;
